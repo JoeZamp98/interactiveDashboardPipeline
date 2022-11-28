efbf1b5e660b778e81fff1f1f98429d0413e985e
JoeZamp98 <joezamp98@gmail.com>
2022-11-27 20:04:00 -0500

    Initial setup in core file.

---
+++ interactiveDashboardCorePipeline.py
@@ -0,0 +1,29 @@
+from boxsdk import JWTAuth, Client
+import copy
+import certifi
+import io
+import pandas as pd
+import pymongo
+import statistics
+import re
+import requests
+
+## -- DEFINE GLOBAL VARIABLES -- ##
+
+specific_survey = 'SURVEY_ID'
+supplemental_survey = 'SURVEY_ID'
+interval = 'DATE/INTERVAL_ID'
+
+## -- ESTABLISH CONNECTION TO MONGODB -- ##
+
+## -- ESTABLISH CONNECTION TO BOX -- ##
+
+## -- CONNECT TO QUALTRICS API AND IMPORT DATA FOR SPECIFIED INSTRUMENTS -- ##
+
+## -- FUNCION CALLS -- ##
+
+# - Connection/Import Functions - # 
+
+# - Structure/Cleaning Functions - #
+
+# - Aggregation Functions - #
